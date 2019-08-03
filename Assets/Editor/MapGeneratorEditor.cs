0d44cd005351e76fa6053c72c974f344ba712f43
jack <jacksporter24@gmail.com>
2019-08-03 13:20:46 -0500

    Adding Editor Script

---
+++ Assets/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,28 @@
+﻿using Scenes.Scripts;
+using UnityEngine;
+using UnityEditor;
+
+namespace Editor
+{
+    [CustomEditor(typeof(MapGenerator))]
+    public class MapGeneratorEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            var mapGen = (MapGenerator) target;
+
+            if (DrawDefaultInspector())
+            {
+                if (mapGen.autoUpdate)
+                {
+                    mapGen.GenerateMap();
+                }
+            }
+
+            if (GUILayout.Button("Generate"))
+            {
+                mapGen.GenerateMap();
+            }
+        }
+    }
+}
