7711f6ad966e86806147504e19c7dd1a4be4b636
jack <jacksporter24@gmail.com>
2019-08-08 19:15:55 -0500

    Seed Tests
    Lerp also throws off testing for scaling as max and min values on the map may change. Need to think about next steps for testing / refactor

--- Assets/Tests/EditModeTests/NoiseTests.cs
+++ Assets/Tests/EditModeTests/NoiseTests.cs
@@ -1,6 +1,6 @@
-﻿using FluentAssertions;
+﻿using ChanceNET;
+using FluentAssertions;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 using Scenes.Scripts;
 using UnityEngine;
 
@@ -8,82 +8,82 @@ namespace Editor.Tests
 {
     public class NoiseTests
     {
-        private readonly Randomizer _randomizer;
+        private readonly Chance _chance;
 
         public NoiseTests()
-        { 
-            _randomizer = new Randomizer();
+        {
+            _chance = new Chance();
         }
         
         [Test]
         public void GivenMapDimensions_WhenCallingGenerateMap_ThenReturnMapWithGivenDimensions()
         {
-            var mapWidth = _randomizer.Next(2, 100);
-            var mapHeight = _randomizer.Next(2, 100);
-            var scale = _randomizer.NextFloat();
-            var octaves = _randomizer.Next(1, 8);
-            var persistence = _randomizer.NextFloat();
-            var lacunarity = _randomizer.NextFloat() + _randomizer.Next(10);
-            var offset = new Vector2(_randomizer.Next(-10000, 10000), _randomizer.Next(-10000, 10000));
-            var seed = _randomizer.Next();
+            var mapWidth = _chance.Integer(2, 100);
+            var mapHeight = _chance.Integer(2, 100);
+            var scale = (float) _chance.Double();
+            var octaves = _chance.Integer(1, 8);
+            var persistence = (float) _chance.Double();
+            var lacunarity = (float) _chance.Double() + _chance.Integer(0, 10);
+            var offset = new Vector2(_chance.Integer(-10000, 10000), _chance.Integer(-10000, 10000));
+            var seed = _chance.Integer();
             
             var result = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale, octaves, persistence, lacunarity, offset, seed);
-            
-            Assert.AreEqual(mapWidth, result.GetLength(0));
-            Assert.AreEqual(mapHeight, result.GetLength(1));
+
+            mapWidth.Should().Be(result.GetLength(0));
+            mapHeight.Should().Be(result.GetLength(1));
         }
 
         [Test]
         public void GivenParameters_WhenCallingGenerateMap_ThenReturnMapWithDifferentValues()
         {
-            var mapWidth = _randomizer.Next(2, 100);
-            var mapHeight = _randomizer.Next(2, 100);
-            var scale = _randomizer.NextFloat();
-            var octaves = _randomizer.Next(1, 8);
-            var persistence = _randomizer.NextFloat();
-            var lacunarity = _randomizer.NextFloat() + _randomizer.Next(10);
-            var offset = new Vector2(_randomizer.Next(-10000, 10000), _randomizer.Next(-10000, 10000));
-            var seed = _randomizer.Next();
+            var mapWidth = _chance.Integer(2, 100);
+            var mapHeight = _chance.Integer(2, 100);
+            var scale = (float) _chance.Double();
+            var octaves = _chance.Integer(1, 8);
+            var persistence = (float) _chance.Double();
+            var lacunarity = (float) _chance.Double() + _chance.Integer(0, 10);
+            var offset = new Vector2(_chance.Integer(-10000, 10000), _chance.Integer(-10000, 10000));
+            var seed = _chance.Integer();
             
             var result = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale, octaves, persistence, lacunarity, offset, seed);
 
             var firstPixel = result[0, 0];
-            var testPixel = result[_randomizer.Next(1, mapWidth), _randomizer.Next(1, mapHeight)];
-            
-            Assert.AreNotEqual(firstPixel, testPixel );
+            var testPixel = result[_chance.Integer(1, mapWidth), _chance.Integer(1, mapHeight)];
+
+            firstPixel.Should().NotBe(testPixel);
         }
 
         [Test]
         public void GivenDifferentScaleValues_WhenCallingGenerateMap_ThenReturnDifferentMaps()
         {
-            var mapWidth = _randomizer.Next(2, 100);
-            var mapHeight = _randomizer.Next(2, 100);
-            var scale1 = _randomizer.NextFloat();
-            var scale2 = _randomizer.NextFloat();
-            var octaves = _randomizer.Next(1, 8);
-            var persistence = _randomizer.NextFloat();
-            var lacunarity = _randomizer.NextFloat() + _randomizer.Next(10);
-            var offset = new Vector2(_randomizer.Next(-10000, 10000), _randomizer.Next(-10000, 10000));
-            var seed = _randomizer.Next();
+            var mapWidth = _chance.Integer(2, 100);
+            var mapHeight = _chance.Integer(2, 100);
+            var scale1 = (float) _chance.Double();
+            var scale2 = (float) _chance.Double();
+            var octaves = _chance.Integer(1, 8);
+            var persistence = (float) _chance.Double();
+            var lacunarity = (float) _chance.Double() + _chance.Integer(0, 10);
+            var offset = new Vector2(_chance.Integer(-10000, 10000), _chance.Integer(-10000, 10000));
+            var seed = _chance.Integer();
             
             var result1 = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale1, octaves, persistence, lacunarity, offset, seed);
             var result2 = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale2, octaves, persistence, lacunarity, offset, seed);
-            
-            Assert.AreNotEqual(result1, result2 );
-            
+
+            Assert.AreNotEqual(result1, result2);
+
         }
 
         [Test]
         public void GivenScaleIsZero_WhenCallingGenerateMap_ShouldReturnNoiseMapWithValidNumbers()
         {
-            var mapWidth = _randomizer.Next(2, 100);
-            var mapHeight = _randomizer.Next(2, 100);
+            var mapWidth = _chance.Integer(2, 100);
+            var mapHeight = _chance.Integer(2, 100);
             var scale = 0;
-            var octaves = _randomizer.Next(1, 8);
-            var persistence = _randomizer.NextFloat();
-            var lacunarity = _randomizer.NextFloat() + _randomizer.Next(10);
-            var offset = new Vector2(_randomizer.Next(-10000, 10000), _randomizer.Next(-10000, 10000));
-            var seed = _randomizer.Next();
+            var octaves = _chance.Integer(1, 8);
+            var persistence = (float) _chance.Double();
+            var lacunarity = (float) _chance.Double() + _chance.Integer(0, 10);
+            var offset = new Vector2(_chance.Integer(-10000, 10000), _chance.Integer(-10000, 10000));
+            var seed = _chance.Integer();
             
             var result = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale, octaves, persistence, lacunarity, offset, seed);
 
@@ -94,31 +94,57 @@ namespace Editor.Tests
         [Test]
         public void GivenGeneratedMap_WhenChangingScale_ShouldScaleFromCenter()
         {
-            true.Should().BeTrue();
+            //lerping messes this up for testing
         }
 
         [Test]
         public void GivenGeneratedMap_WhenChangingXOffset_ShouldMoveMap()
         {
-            true.Should().BeTrue();
+            //lerping changes values more than should be. could test all points are relatively the same between scales
         }
 
         [Test]
         public void GivenGeneratedMap_WhenChangingYOffset_ShouldMoveMap()
         {
-            true.Should().BeTrue();
+            //lerping changes values more than should be. could test all points are relatively the same between scales
         }
 
         [Test]
-        public void GivenGeneratedMap_WhenChangingSeed_ShouldGenerateNewMap()
+        public void GivenGeneratedMap_WhenChangingOnlySeed_ShouldGenerateNewMap()
         {
-            true.Should().BeTrue();
+            var mapWidth = _chance.Integer(2, 100);
+            var mapHeight = _chance.Integer(2, 100);
+            var scale = (float) _chance.Double() + _chance.Integer(0, 10);
+            var octaves = _chance.Integer(1, 8);
+            var persistence = (float) _chance.Double();
+            var lacunarity = (float) _chance.Double() + _chance.Integer(0, 10);
+            var offset = new Vector2(_chance.Integer(-10000, 10000), _chance.Integer(-10000, 10000));
+            var seed = _chance.Integer();
+            
+            var result1 = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale, octaves, persistence, lacunarity, offset, seed);
+
+            seed = _chance.Integer();
+            var result2 = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale, octaves, persistence, lacunarity, offset, seed);
+
+            Assert.AreNotEqual(result1, result2);
         }
 
         [Test]
         public void GivenConstantSeed_WhenRegeneratingMap_ShouldGenerateSameMap()
         {
-            true.Should().BeTrue();
+            var mapWidth = _chance.Integer(2, 100);
+            var mapHeight = _chance.Integer(2, 100);
+            var scale = (float) _chance.Double() + _chance.Integer(0, 10);
+            var octaves = _chance.Integer(1, 8);
+            var persistence = (float) _chance.Double();
+            var lacunarity = (float) _chance.Double() + _chance.Integer(0, 10);
+            var offset = new Vector2(_chance.Integer(-10000, 10000), _chance.Integer(-10000, 10000));
+            var seed = _chance.Integer();
+            
+            var result1 = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale, octaves, persistence, lacunarity, offset, seed);
+            var result2 = Noise.GenerateNoiseMap(mapWidth, mapHeight, scale, octaves, persistence, lacunarity, offset, seed);
+            
+            Assert.AreEqual(result1, result2);
         }
         
         
